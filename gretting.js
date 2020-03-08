e6577023e4e09f8281a08b69bd1bc7bf7ec0bb8c
Hwisha <31067658+Hwisha@users.noreply.github.com>
2020-03-08 23:15:26 +0900

    version3

---
+++ gretting.js
@@ -0,0 +1,45 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
+
+function saveName(text){
+    localStorage.setItem(USER_Name, text);
+}
+
+
+const USER_Name = "currentUser",
+    SHOWING = "showing";
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING);
+    form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING); 
+    greeting.classList.add(SHOWING);
+    greeting.innerText = `Hello ${text}`
+}
+
+function loadName(){
+    const currentUser = localStorage.getItem(USER_Name);
+    if(currentUser === null){
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    }
+}
+
+
+function init(){
+    loadName();
+}
+
+init();
---
+++ weather.js
@@ -0,0 +1,53 @@
+const weather = document.querySelector(".js-weather");
+
+const API_KEY = "841579dc1de24b9adc4000b729b73aa0"
+const COORDS = 'coords';
+
+function getWeather(lat, lon){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`).then(function(response){
+        return response.json();
+    }).then(function(json){
+        const temp = json.main.temp;
+        const place = json.name;
+        weather.innerText =  `${temp} @ ${place}`
+    })
+}
+
+function handleGeoError(){
+    console.log("error");
+}
+
+function saveCoords(coordsObj){
+    localStorage.setItem(COORDS, JSON.stringify(coordsObj));
+}
+
+function handleGeoSucces(position){
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    const coordsObj = {
+        latitude,
+        longitude
+    };
+    saveCoords(coordsObj);
+    getWeather(latitude, longitude);
+    
+}
+function askForCoords(){
+    navigator.geolocation.getCurrentPosition(handleGeoSucces, handleGeoError);
+}
+
+function loadCoords(){
+    const loadedCoords = localStorage.getItem(COORDS);
+    if(loadedCoords === null){
+        askForCoords();
+    } else {
+        const parseCoords = JSON.parse(loadedCoords);
+        getWeather(parseCoords.latitude, parseCoords.longitude);
+    }
+}
+
+function init(){
+    loadCoords();
+}
+
+init();
